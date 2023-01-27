945f4c6709df379e5f60df62dfe347a763ef6ea1
152348 <87729129+152348@users.noreply.github.com>
2023-01-27 21:14:26 +0100

    Add files via upload

---
+++ inverted_index.py
@@ -0,0 +1,31 @@
+from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
+from whoosh.analysis import StemmingAnalyzer
+import os, os.path
+from whoosh import index
+
+class Inverted_index():
+
+    schema = Schema(title=ID(stored=True,lowercase=True),
+                    user=ID(stored=True),
+                    review=TEXT(analyzer=StemmingAnalyzer()),
+                    _review=STORED,
+                    sentiment=ID)
+                    
+    def __init__(self, index_dir):
+        """Inizializzatore a cui deve essere passato il nome della directory 
+            in cui creare o aprire l'inverted index"""
+        _index_dir = index_dir
+
+    def create_index(self):
+        """Metodo che si occupa di creare l'inverted index con i campi
+            specificati dalla variabile schema"""
+        if not os.path.exists(self._index_dir):
+            os.mkdir(self._index_dir)
+
+        ix = index.create_in(self._index_dir, Inverted_index.schema)
+
+    def index_documents(self):
+        pass
+
+    def open_index(self):
+        pass
