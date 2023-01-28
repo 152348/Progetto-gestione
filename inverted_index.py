b36572b057e5c583cd5498fafe2bb37408ed287f
152348 <87729129+152348@users.noreply.github.com>
2023-01-28 13:28:44 +0100

    Add files via upload

--- inverted_index.py
+++ inverted_index.py
@@ -1,20 +1,21 @@
-from whoosh.fields import Schema, TEXT, ID, STORED
+from whoosh.fields import Schema, TEXT, ID
 from whoosh.analysis import StemmingAnalyzer
 import os, os.path
 from whoosh import index
+import csv
+from whoosh.qparser import MultifieldParser
 
 class Inverted_index():
 
-    schema = Schema(title=ID(stored=True,lowercase=True),
+    schema = Schema(title=ID(stored=True),
                     user=ID(stored=True),
-                    review=TEXT(analyzer=StemmingAnalyzer()),
-                    _review=STORED,
+                    review=TEXT(analyzer=StemmingAnalyzer(), stored=True),
                     sentiment=ID)
                     
     def __init__(self, index_dir):
         """Inizializzatore a cui deve essere passato il nome della directory 
             in cui creare o aprire l'inverted index"""
-        _index_dir = index_dir
+        self._index_dir = index_dir
 
     def create_index(self):
         """Metodo che si occupa di creare l'inverted index con i campi
@@ -22,10 +23,48 @@ class Inverted_index():
         if not os.path.exists(self._index_dir):
             os.mkdir(self._index_dir)
 
-        ix = index.create_in(self._index_dir, Inverted_index.schema)
+        index.create_in(self._index_dir, Inverted_index.schema)
 
-    def index_documents(self):
-        pass
+    def index_documents(self, n_break):
+        if not os.path.exists(self._index_dir):
+            print("La cartella dell'indice non esiste, esegui prima create_index")
+        else:
+            ix = index.open_dir(self._index_dir)
+            writer = ix.writer()
 
-    def open_index(self):
-        pass
+            try:
+                with open('csv_count.txt', 'x') as number:
+                    number.write(str(1))
+            except FileExistsError:
+                pass
+
+            with open('csv_count.txt', 'r') as number:
+                csv_counter = int(number.read())
+
+            with open('Reviews_MAL.csv', 'r', encoding='utf8') as rev:
+                df = csv.DictReader(rev)
+                for row in range(csv_counter):
+                    next(df)
+                for line in df:
+                    writer.add_document(title=line['Title'], user=line['User'], review=line['Text'], _stored_review=line['Text'])
+                    csv_counter += 1
+                    if csv_counter == n_break:
+                        break
+
+            with open('csv_count.txt', 'w') as number:
+                number.write(str(csv_counter))
+
+            writer.commit()
+
+    def search(self, query):
+        ix = index.open_dir(self._index_dir)
+        with ix.searcher() as s:
+            parser = MultifieldParser(['title', 'user', 'review', 'sentiment'], schema = Inverted_index.schema)
+            parsed_q = parser.parse(query)
+            results = s.search(parsed_q)
+            if len(results) == 0:
+                print("No results found")
+            else:
+                for result in results:
+                    print(result)
+
