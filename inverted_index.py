fe1abb2ba793d538397de9301355619d02d51aa3
152348 <87729129+152348@users.noreply.github.com>
2023-01-28 16:22:57 +0100

    Add files via upload

--- inverted_index.py
+++ inverted_index.py
@@ -49,7 +49,6 @@ class Inverted_index():
                 break_counter = 0
                 df = csv.DictReader(rev)
                 for row in range(csv_counter):
-                    print(row)
                     next(df)
                 for line in df:
                     writer.add_document(title=line['Title'].lower(), user=line['User'], review=line['Text'], _stored_review=line['Text'])
