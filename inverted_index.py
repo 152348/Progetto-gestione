52ce3e95a6c45ad817f0ba5caf9193c5beec38c1
152348 <87729129+152348@users.noreply.github.com>
2023-01-28 15:38:14 +0100

    Add files via upload

--- inverted_index.py
+++ inverted_index.py
@@ -25,7 +25,11 @@ class Inverted_index():
 
         index.create_in(self._index_dir, Inverted_index.schema)
 
-    def index_documents(self, n_break):
+    def index_documents(self, n_break=-1):
+        """Metodo che si occupa di indicizzare i documenti contenuti nel file csv Reviews_MAL.csv. Essendo
+        un'operazione lunga, è possibile passare come parametro il numero di documenti da indicizzare
+        per questa iterazione del metodo. Il metodo non riparte ogni volta dalla prima recensione ma salva 
+        in un file a quale recensione era arrivato."""
         if not os.path.exists(self._index_dir):
             print("La cartella dell'indice non esiste, esegui prima create_index")
         else:
@@ -42,21 +46,24 @@ class Inverted_index():
                 csv_counter = int(number.read())
 
             with open('Reviews_MAL.csv', 'r', encoding='utf8') as rev:
+                break_counter = 0
                 df = csv.DictReader(rev)
                 for row in range(csv_counter):
                     next(df)
                 for line in df:
-                    writer.add_document(title=line['Title'], user=line['User'], review=line['Text'], _stored_review=line['Text'])
+                    writer.add_document(title=line['Title'].lower(), user=line['User'], review=line['Text'], _stored_review=line['Text'])
                     csv_counter += 1
-                    if csv_counter == n_break:
+                    break_counter +=1
+                    with open('csv_count.txt', 'w') as number:
+                        number.write(str(csv_counter))
+                    if break_counter != 1 and break_counter == n_break:
                         break
 
-            with open('csv_count.txt', 'w') as number:
-                number.write(str(csv_counter))
-
-            writer.commit()
+            writer.commit(optimize=True)
 
     def search(self, query):
+        """Metodo che si occupa di parsare la query passata come parametro dall'utente, cercarla e 
+           stampare i risultati"""
         ix = index.open_dir(self._index_dir)
         with ix.searcher() as s:
             parser = MultifieldParser(['title', 'user', 'review', 'sentiment'], schema = Inverted_index.schema)
