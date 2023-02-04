c40896b7adf4730043985cc5ce7f04151f6e673c
152348 <87729129+152348@users.noreply.github.com>
2023-02-04 11:18:16 +0100

    Add files via upload

--- inverted_index.py
+++ inverted_index.py
@@ -9,6 +9,7 @@ from whoosh import scoring
 from sentiment import sentiment_analysis
 from whoosh.highlight import UppercaseFormatter
 
+
 class Inverted_index():
 
     schema = Schema(title=ID(stored=True),
@@ -17,6 +18,8 @@ class Inverted_index():
                     sentiment_roberta=ID(stored=True),
                     sentiment_amazon=ID(stored=True),
                     sentiment_nltk=ID(stored=True))
+    
+    models = {'TF_IDF':scoring.TF_IDF(), 'BM25':scoring.BM25F(), 'Frequency':scoring.Frequency()}
                     
     def __init__(self, index_dir):
         """Inizializzatore a cui deve essere passato il nome della directory 
@@ -70,23 +73,35 @@ class Inverted_index():
 
             writer.commit(optimize=True)
 
-    def search(self, query):
-        """Metodo che si occupa di parsare la query passata come parametro dall'utente, cercarla e 
+    def search(self, query, m_choice):
+        """Metodo che si occupa di parsare la query passata come parametro dall'utente, cercarla e
            stampare i risultati"""
         ix = index.open_dir(self._index_dir)
-        with ix.searcher() as s:
+        with ix.searcher(weighting=Inverted_index.models[m_choice]) as s:
             og = qparser.OrGroup.factory(0.9)
-            parser = MultifieldParser(['title', 'user', 'review', 'sentiment_roberta', 'sentiment_amazon', 'sentiment_nltk'],
-                schema = Inverted_index.schema, group = og)
+            parser = MultifieldParser(
+                ['title', 'user', 'review', 'sentiment_roberta', 'sentiment_amazon', 'sentiment_nltk'],
+                schema=Inverted_index.schema, group=og)
             parsed_q = parser.parse(query)
-            results = s.search(parsed_q, terms= True)
-            #results.formatter = UppercaseFormatter(between="\n")
-            if len(results) == 0:
-                print("No results found")
+            results = s.search(parsed_q, terms=True)
+            # results.formatter = UppercaseFormatter(between="\n")
+            totale = len(results)
+            if totale == 0:
+                print("\nNo results found\n")
             else:
+                count = 1
                 for hit in results:
+                    print("\n------------------ RISULTATO ", count, "SU ", totale, "TOTALI ------------------\n\n")
                     print(f"Title: {hit['title']}, User: {hit['user']}, Sentiments: "
-                    f"{hit['sentiment_roberta']}-{hit['sentiment_amazon']}-{hit['sentiment_nltk']}, Review: {hit['review']}\n")
-                #for hit in results:
-                    #print(hit.highlights("review"))
-
+                          f"{hit['sentiment_roberta']} - {hit['sentiment_amazon']} - {hit['sentiment_nltk']}\nReview: {hit['review']}\n")
+                    spostamento = input("-premere 1 per passare al prossimo risultato\n-premere 2 per tornare al menù\n")
+                    if spostamento == '1':
+                        count += 1
+                        continue
+                    elif spostamento == '2':
+                        break
+                    else:
+                        print("tasto non disponibile...")
+                if count == totale + 1:
+                    print("LISTA FINITA...\n")
+                print("ritorno al menù...\n")
