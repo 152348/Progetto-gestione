49b1ba08fb4e81ec2445e6cde6bc5ce4124a7bab
152348 <87729129+152348@users.noreply.github.com>
2023-02-12 18:32:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+"""Module that implements the terminal user interface of the search engine. The user can write queries and navigate through
+the results, change the information retrieval model, create a new inverted index, index an arbitrary number of documents and
+try a set of ten benchmark queries for which the user can give a relevance value to calculate the DCG.
+
+Parameters:
+    index: instance of the inverted index class
+    model: default IR model used by the search engine
+    bench_uin: benchmark queries expressed in natural language
+    bench_query: benchmark queries expressed with the query language of the search engine
+"""
 from inverted_index import Inverted_index
 from math import log2
 
-def hit_print(hit,count,length):
-    print("\n------------------ RESULT ", count, " OUT OF ",length," ------------------\n\n")
+def hit_print(hit,position,length):
+    """Function to pretty-print the results of query search. For every results, the function prints the ranking number of the 
+    review out of the total reviews retrieved, the title of the manga, the user who wrote the review, the results of three 
+    sentiment analysis methods performed on the reviews separated by a dash and finally the review itself"""
+    print("\n------------------ RESULT ", position, " OUT OF ",length," ------------------\n\n")
     print(f"Title: {hit['title']}, User: {hit['user']}, Sentiments: "
             f"{hit['sentiment_roberta']} - {hit['sentiment_amazon']} - {hit['sentiment_nltk']}\nReview: {hit['review']}\n")
 
 def DCG_calc(relevances):
+    """Function that given a list of integers, calculates the Dispositioned cumulative gain using a binary logarithm. Every
+    relevance value is divided by the binary logarithm of the ranking position of the review it represents. The function
+    assumes that the position in the list reflects the position in the ranking (so the first the value corresponds to the
+    first review in the ranking and so on)"""
     DCG = relevances[0]
     for i in range(1,len(relevances)):
         DCG += relevances[i] / log2(i+1)
@@ -38,28 +55,32 @@ while True:
     print("-Press 4 to index some documents\n-Press 5 to try the benchmark queries\n-Press 6 to exit")
     print('-------------------------------------')
     choice = input('Choice: ')
+
     if choice == '1':
         query = input("\nEnter the query...\n")
         results = index.search(query, model)
-        length = next(results)
+        length = next(results) #variable that stores the total number of documents retrieved
         if length == 0:
             print("\nNo results found\n")
         else:
-            count = 1
+            position = 1 #variable that stores the position of the current document in the ranking
             for hit in results:
-                hit_print(hit, count, length)
-                spostamento = input("-Press 1 to proceed to the next result\n-Press 2 to return to the menù\n")
-                if spostamento == '1':
-                    count += 1
+                hit_print(hit, position, length)
+                progress = input("\n-Press 1 to proceed to the next result\n-Press 2 to return to the menù\n")
+                if progress == '1':
+                    position += 1
                     continue
-                elif spostamento == '2':
+                elif progress == '2':
+                    position += 1
                     break
                 else:
+                    position += 1
                     print("Key not available...")
                     break
-            if count >= 11:
+            if position >= 11:
                 print("\nEnd of list..")
             print("\nReturning to the menù...")
+
     elif choice == '2':
         print('Available models:\n1)BM25\n2)TF_IDF\n3)Frequency')
         n_model = input('\nEnter what model you want: ')
@@ -71,13 +92,23 @@ while True:
             model = 'Frequency'
         else:
             print('Model not present\n')
+
     elif choice == '3':
         confirm = input('Are you sure you want to create a new index?: ')
         if confirm.lower() == 'yes':
             index.create_index()
+
     elif choice == '4':
-        num_ind = int(input('Enter the number of documents you want to index: '))
-        index.index_documents(num_ind)
+        try:
+            ind_num = int(input('Enter the number of documents you want to index: '))
+        except ValueError:
+            print("\nYou have to enter a positive value!")
+            continue
+        if ind_num <= 0 :
+            print("\nYou have to enter a positive value!")
+            continue
+        index.index_documents(ind_num)
+
     elif choice == '5':
         print("Available benchmark queries:\n")
         for i,query in enumerate(bench_uin):
@@ -99,41 +130,43 @@ while True:
         else:
             print("\nChoice not supported... DCG will not be calculated")
             dcg_choice = False
-        results = index.search(bench_query[q_choice], 'BM25')
+        results = index.search(bench_query[q_choice], model)
         length = next(results)
-        count = 1
-        relevances = []
+        position = 1
+        relevances = [] #list that will store the relevance values decided by the user
         for hit in results:
-            hit_print(hit, count, length)
+            hit_print(hit, position, length)
             if dcg_choice:
                 try:
                     relevance = int(input("\nWhat relevance do you think this result has with regards to the UIN " 
                     "on a scale of 1 to 3?: "))
                 except ValueError:
-                    print("Incorrect relevance value, exiting the benchark...\n")
+                    print("\nIncorrect relevance value, exiting the benchark...")
                     break
                 if relevance < 1 or relevance > 3:
-                    print("Incorrect relevance value, exiting the benchmark...\n")
+                    print("\nIncorrect relevance value, exiting the benchmark...")
                     break
                 relevances.append(relevance)
-            spostamento = input("\n-Press 1 to proceed to the next result\n-Press 2 to return to the menù\n")
-            if spostamento == '1':
-                count += 1
+            progress = input("\n-Press 1 to proceed to the next result\n-Press 2 to return to the menù\n")
+            if progress == '1':
+                position += 1
                 continue
-            elif spostamento == '2':
-                count += 1
+            elif progress == '2':
+                position += 1
                 break
             else:
-                count += 1
+                position += 1
                 print("Key not available...\n")
                 break
-        if count >= 11:
+        if position >= 11:
             print("\nEnd of list..")
             if dcg_choice:
                 print("\nThe DCG is", DCG_calc(relevances))
         print("\nReturning to the menù...")
+
     elif choice == '6':
         print("Exiting...")
         break
+
     else:
         print("\nKey not available...") 
